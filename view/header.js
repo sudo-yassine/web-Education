804bb019d592651da3e1e7d526e3740a3bea898c
yassin-esprit <yassin.jaouani@esprit.tn>
2024-03-17 14:39:17 +0100

    home page

---
+++ view/header.js
@@ -0,0 +1,10 @@
+var cpCreditsUrl = "https://codepen.io/PickJBennett/details/BdbrMW";
+var cpCreditsTitle = "🌈 Sexy Animated Rainbow Waves Header";
+var cpCreditsTwitter = document.getElementById("js-tweet-this");
+cpCreditsTwitter.href =
+  "https://twitter.com/intent/tweet?text=" +
+  encodeURI(cpCreditsTitle) +
+  "&url=" +
+  encodeURI(cpCreditsUrl) +
+  "&via=PickJBennett&related=PickJBennett,CiaoFileManager";
+cpCreditsTwitter.innerHTML += "Tweet This Pen";
