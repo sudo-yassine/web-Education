eaad3b02a6e1cefec8ae865d9c74903b017d0b4f
yassin-esprit <yassin-esprit>
2024-03-20 19:26:48 +0100

    ajout

--- view/listcours.php
+++ view/listcours.php
@@ -15,60 +15,64 @@ $list = $coursC->listcours();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>cours</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!-- Font Awesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Datatables CSS  -->
+    <link href="https://cdn.datatables.net/v/bs5/dt-1.13.4/datatables.min.css" rel="stylesheet" />
+    <!-- CSS  -->
+    <link rel="stylesheet" href="./css/style.css">
     </head>
 <body>
-    <?php
+    <!-- <?php
     // Include the header content
     // include 'Header.php';
-    ?>
+    ?> -->
 
     <div class="container">
-        <div >
-            <div >
-                <span >tous les cours</span>
+        <div class="d-flex justify-content-between align-items-center mb-3 mt-5" >
+            <div class="text-body-secondary">
+                <span class="h5">tous les cours</span>
                 <br>
                crud cours 
             </div>
-            <!-- Button to trigger Add student offcanvas -->
-            <button type="button" data-bs-toggle="modal" data-bs-target="#addStudentModal">
+            <button class="btn btn-dark" type="button" data-bs-toggle="modal" data-bs-target="#addcoursModal">
                 ajouter un cours
             </button>
         </div>
 
         <!-- Modal -->
-        <div  id="addStudentModal" tabindex="-1" aria-labelledby="addStudentModalLabel" aria-hidden="true">
+        <div class="modal fade"  id="addcoursModal" tabindex="-1" aria-labelledby="addcoursModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 id="addStudentModalLabel">ajouter un cours</h5>
+                        <h5 id="addcoursModalLabel">ajouter un cours</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div 
-                        <div id="error">
-                            <?php echo $error; ?>
-                        </div>
+                    <div class="modal-body">
                         <form method="POST" id="insertForm" action="addcours.php" enctype="multipart/form-data">
-                            <div >
-                                <div>
-                                    <label >nom cours</label>
-                                    <input type="text"  name="nom_cours" placeholder="nom du cours">
+                            <div class="row mb-3">
+                                <div class="col">
+                                    <label class="form-label">nom cours</label>
+                                    <input type="text"class="form-control"  name="nom_cours" placeholder="nom du cours">
                                 </div>
-                                <div>
-                                    <label >heures</label>
-                                    <input type="text"  name="heurs" placeholder="nombre heures">
+                                <div class="col">
+                                    <label class="form-label" >heures</label>
+                                    <input type="text" class="form-control" name="heures" placeholder="nombre heures">
                                 </div>
                                 <div>
                                     <label >niveau</label>
-                                    <input type="text" name="niveau" placeholder="niveau">
+                                    <input type="text" class="form-control" name="niveau" placeholder="niveau">
                                 </div>
-                                <div>
+                                <div class="form-label">
                                     <label >contenu</label>
-                                    <input type="text"  name="contenu" placeholder="contenu">
+                                    <input type="text"class="form-control"  name="contenu" placeholder="contenu">
                                 </div>
                             </div>
                             <div>
-                                <button type="submit"  id="insertBtn">Submit</button>
-                                <button type="button"  data-bs-dismiss="modal">Cancel</button>
+                                <button type="submit"  class="btn btn-primary me-1" id="insertBtn">Submit</button>
+                                <button type="button"  class="btn btn-primary me-1" data-bs-dismiss="modal">Cancel</button>
                             </div>
                         </form>
                     </div>
@@ -101,8 +105,8 @@ $list = $coursC->listcours();
                         
                         <td align="center">
 
-                            <a href="updatecours.php?id=<?php echo $student['id_cours']; ?>"><i></i>Update</a>
-                            <a href="deletecours.php?id=<?php echo $cours['id_cours']; ?>"><i ></i>Delete</a>
+                            <!-- <a href="updatecours.php?id=<?php echo $cours['id_cours']; ?>"><i></i>Update</a> -->
+                            <a href="deletecours.php?id_cours=<?php echo $cours['id_cours']; ?>"class="btn"><i class="fa-solid fa-trash fa-xl"></i>Delete</a>
 
                         </td>
                     </tr>
@@ -112,8 +116,21 @@ $list = $coursC->listcours();
             </tbody>
         </table>
     </div>
-
-
+  <!-- Bootstrap  -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <!-- Jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- Datatables  -->
+    <script src="https://cdn.datatables.net/v/bs5/dt-1.13.4/datatables.min.js"></script>
+    <!-- JS  -->
+    <script>
+        // function to display image before upload
+        $("input.image").change(function() {
+            var file = this.files[0];
+            var url = URL.createObjectURL(file);
+            $(this).closest(".row").find(".preview_img").attr("src", url);
+        });
+    </script>
 
 </body>
 
