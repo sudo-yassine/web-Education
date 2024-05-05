d6d7c0cf5b5d613e40d4a31801e6a18582de9283
Nada1231 <nada.hsayri@esprit.tn>
2024-05-05 13:54:08 +0100

    mail

--- view/AddAdmin.php
+++ view/AddAdmin.php
@@ -1,22 +1,52 @@
 <?php
 include '../Controller/adminC.php';
+//include '../config.php'; // Assurez-vous que ceci inclut les configurations pour PHPMailer
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
 
 $adminC = new adminC();
 
 // Vérifier si les données POST sont présentes ou si elles viennent de Facebook (AJAX)
-if(isset($_POST['niveau']) && (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['pass']) && isset($_POST['Email'])) || isset($_POST['facebookData'])) {
+if(isset($_POST['niveau']) && (isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['pass']) && isset($_POST['email'])) || isset($_POST['facebookData'])) {
     if(isset($_POST['facebookData'])) {
         // Traiter les données venant de Facebook
         $data = json_decode($_POST['facebookData']);
         $admin = new admin(null, 'niveau_par_defaut', $data->nom, $data->prenom, 'mot_de_passe_par_defaut', $data->Email);
     } else {
         // Créer une nouvelle instance de la classe admin avec les données POST classiques
-        $admin = new admin(null, $_POST['niveau'], $_POST['nom'], $_POST['prenom'], $_POST['pass'], $_POST['Email']);
+        $admin = new admin(null, $_POST['niveau'], $_POST['nom'], $_POST['prenom'], $_POST['pass'], $_POST['email']);
     }
     
     $success = $adminC->addAdmin($admin);
 
     if($success) {
+        // Envoyer l'email de confirmation
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'nadahsayri99@gmail.com';
+            $mail->Password = 'ekbp ynqu ehor gkfn';
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = 465;
+            $mail->setFrom('nadahsayri99@gmail.com', 'Wisdom Wave');
+            $mail->addAddress($_POST['email']);  // Utiliser l'adresse e-mail de l'admin ajouté
+
+            $mail->isHTML(true);
+            $mail->Subject = 'Confirmation de création de compte';
+            $mail->Body = "Bonjour {$_POST['prenom']},<br><br>Votre compte administrateur a été créé avec succès.<br>Cordialement,<br>L'équipe de Wisdom Wave";
+
+            $mail->send();
+        } catch (Exception $e) {
+            echo "Erreur d'envoi de l'e-mail : {$mail->ErrorInfo}";
+        }
+        
         header('Location: listAdmins.php');
         exit();
     } else {
--- view/sendemail.php
+++ view/sendemail.php
@@ -1,43 +1,31 @@
 <?php
-require_once 'C:\xampp\htdocs\admin\web-Education\View\PHPMailer\src\SMTP.php';
-require_once 'C:\xampp\htdocs\admin\web-Education\View\PHPMailer\src\PHPMailer.php';
-require_once 'C:\xampp\htdocs\admin\web-Education\View\PHPMailer\src\Exception.php';
+include '../config1.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email'], $_POST['name'], $_POST['password'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);  // Hash du mot de passe
-
-    // Enregistrer ces données dans une base de données (non inclus ici)
-
-    // Envoi d'un email de confirmation
+if (isset($_POST["email"])) {
     $mail = new PHPMailer(true);
-
-    try {
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'nadahsayri99@gmail.com';  // Votre adresse Gmail
-        $mail->Password = 'dsei xpan kuhy iigh';    // Votre mot de passe Gmail
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port = 587;
-
-        $mail->setFrom('nadahsayri99@gmail.com', 'WisdomWave');
-        $mail->addAddress($email);
-
-        $mail->isHTML(true);
-        $mail->Subject = 'Bienvenue sur notre site !';
-        $mail->Body = '<h1>Bienvenue ' . $name . ' !</h1><p>Merci de vous être inscrit.</p>';
-
-        $mail->send();
-        echo 'Inscription réussie, email de confirmation envoyé.';
-    } catch (Exception $e) {
-        echo 'Erreur lors de l\'envoi de l\'email: ' . $mail->ErrorInfo;
-    }
-} else {
-    echo 'Données du formulaire manquantes.';
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'nadahsayri99@gmail.com';
+    $mail->Password = 'ekbp ynqu ehor gkfn';
+    $mail->SMTPSecure = 'tls';
+   $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port = 465;
+    $mail->setFrom('nadahsayri99@gmail.com');
+  //$mail->addAddress('nadahsayri99@gmail.com');
+  $email_p = ($_POST['email']);
+  $mail->addAddress($email_p);
+    $mail->isHTML(true);
+    $mail->Subject = 'Reponse de votre reclamation';
+    $mail->Body = 'Bonjour Mr/Mme,<br><br>' . $_POST["messagerep"] .'<br>Cordialement,<br>L\'équipe de TUNISTYLE';
+    
+    // Send the email
+    $mail->send();
 }
 ?>
