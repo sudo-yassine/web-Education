c025a5c15c0c355d4fb6c43c2741cd1e66231bb5
AhmedBouhlel69 <bouhlel.ahmed@esprit.tn>
2024-05-11 18:59:07 +0100

    reclamation

--- view/ListReclamation.php
+++ view/ListReclamation.php
@@ -117,7 +117,7 @@ $list = $reclamationC->ListReclamtion();
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id_reclamationement="addreclamationModalLabel">Ajout reclamation</h5>
+                        <h5 class="modal-title" id_reclamation="addreclamationModalLabel">Ajout reclamation</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
